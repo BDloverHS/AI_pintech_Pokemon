3b56c9b0a7464205dcdade136823aec790007df7
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-13 10:30:26 +0900

    기본 세팅1

---
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -0,0 +1,19 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    /**
+     * 정적 경로 설정(CSS, JS, 이미지 자원 등)
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // resources 폴더 안에 있는 static 폴더를 정적 경로 설정함
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static");
+    }
+}
