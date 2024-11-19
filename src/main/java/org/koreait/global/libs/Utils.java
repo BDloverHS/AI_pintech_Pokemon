0fa10106a13b9a1314fe5f46e8fd17a35bb8c288
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-19 15:46:13 +0900

    회원가입과 로그인 구성

--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -2,13 +2,17 @@ package org.koreait.global.libs;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
 public class Utils {
 
     private final HttpServletRequest request;
+    private final MessageSource messageSource;
 
     public boolean isMobile() {
         // 요청 헤더 쪽의 User-Agent에서 브라우저 정보를 가져옴
@@ -29,5 +33,17 @@ public class Utils {
         String prefix = isMobile() ? "mobile" : "front";
 
         return String.format("%s/%s", prefix, path);
+    }
+
+    /**
+     * 메세지 코드로 조회된 문구
+     *
+     * @param code
+     * @return
+     */
+    public String getMessage(String code) {
+        Locale lo = request.getLocale(); // 사용자 요청 헤더(Accept-Language)
+
+        return messageSource.getMessage(code, null, lo);
     }
 }
