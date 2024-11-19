1370c261dbf973923fd91a4ff00304d5850eacf2
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-19 16:47:35 +0900

    회원가입과 로그인 구성

--- src/main/java/org/koreait/member/controllers/RequestAgree.java
+++ src/main/java/org/koreait/member/controllers/RequestAgree.java
@@ -1,5 +1,6 @@
 package org.koreait.member.controllers;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -7,8 +8,14 @@ import java.util.List;
 
 @Data
 public class RequestAgree {
-    @NotNull
-    private boolean[] requiredTerms; // 필수 약관 동의 여부 / 반드시 모두 선택을 해야하므로 갯수 체크만으로도 충분
+    @AssertTrue
+    private boolean requiredTerms1;
+
+    @AssertTrue
+    private boolean requiredTerms2;
+
+    @AssertTrue
+    private boolean requiredTerms3;
 
     private List<String> optionalTerms; // 선택 약관 동의 여부 / 선택 약관은 어떤 약관인지 구분할 수 있어야 함
 }
