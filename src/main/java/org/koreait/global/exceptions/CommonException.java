5f0f7be13390b9acf5fbb5d5dbb13cad986df8e8
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-21 16:15:34 +0900

    회원가입 유효성

--- src/main/java/org/koreait/global/exceptions/CommonException.java
+++ src/main/java/org/koreait/global/exceptions/CommonException.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.util.Objects;
+
 /**
  * 모든 사용자 정의 예외 상위 클래스
  *
@@ -15,6 +17,7 @@ public class CommonException extends RuntimeException {
     private boolean errorCode;
 
     public CommonException(String message, HttpStatus status) {
-        this.status = status;
+        super(message);
+        this.status = Objects.requireNonNullElse(status, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
