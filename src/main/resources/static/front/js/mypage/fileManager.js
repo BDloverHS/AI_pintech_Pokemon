0acf5a9309cb32cfcd2ef76fef31925f3b90d01c
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-06 17:16:59 +0900

    js

--- src/main/resources/static/front/js/mypage/fileManager.js
+++ src/main/resources/static/front/js/mypage/fileManager.js
@@ -1,3 +1,15 @@
+var commonLib = commonLib ?? {};
+commonLib.fileManager = {
+    /**
+    * 파일 업로드 처리
+    *
+    *
+    */
+    upload(files, gid, location, single, imageOnly) {
+
+    }
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     const fileUploads = document.getElementsByClassName("file-upload");
     const fileEl = document.createElement("input")
@@ -5,7 +17,26 @@ window.addEventListener("DOMContentLoaded", function() {
 
     for (const el of fileUploads) {
         el.addEventListener("click", function() {
+            const {gid, location, single, imageOnly} = this.dataset;
+
+            fileEl.gid = gid;
+            fileEl.location = location;
+            fileEl.imageOnly = imageOnly === 'true';
+            fileEl.single = single === 'true';
+            fileEl.multiple = !fileEl.single; // false - 단일 파일 선택, true - 여러 파일 선택 가능
+
             fileEl.click();
         });
     }
+
+    // 파일 선택 시 - chage 이벤트 발생
+    fileEl.addEventListener("change", function(e){
+        // e.currentTarget, e.target
+        // console.dir(e.currentTarget.files)
+       const files = e.currentTarget.files;
+       const {gid, location, single, imageOnly} = fileEl;
+
+       const { fileManager } = commonLib;
+       fileManager.upload(files, gid, location, single, imageOnly);
+    });
 });
