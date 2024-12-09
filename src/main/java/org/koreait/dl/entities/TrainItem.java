d982de4ff4c089ba048da41eadd03207eaf8361e
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-09 11:19:26 +0900

    fileManager.js 수정(전송 양식 만들기 추가)

--- src/main/java/org/koreait/dl/entities/TrainItem.java
+++ src/main/java/org/koreait/dl/entities/TrainItem.java
@@ -1,10 +1,8 @@
 package org.koreait.dl.entities;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import lombok.*;
 import org.koreait.global.entities.BaseEntity;
 
 @Data
@@ -13,6 +11,8 @@ import org.koreait.global.entities.BaseEntity;
 @NoArgsConstructor
 @AllArgsConstructor // 빌더 패턴일 때 기본 생성자가 접근 가능해야 하는 경우. NoArgsConstructor와 같이 사용하여야 함.
 public class TrainItem extends BaseEntity {
+    @Id @Generated
+    private Long seq;
     private int item1;
     private int item2;
     private int item3;
@@ -23,6 +23,7 @@ public class TrainItem extends BaseEntity {
     private int item8;
     private int item9;
     private int item10;
-    private int result; // 무조건 정수 범위. 여기선 0~4로 나눌 예정임
+    private int result; // 무조건 정수 범위.
+
 
 }
