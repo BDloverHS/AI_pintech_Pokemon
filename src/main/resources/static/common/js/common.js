a55b3a31f85d8d4d6cf967f9c10ca56653b641de
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-09 12:54:25 +0900

    fileManager.js 오류 고침 + common.js 만드는 중

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,3 +1,65 @@
+var commonLib = commonLib ?? {};
+
+/**
+ * 메타 태그 정보 조회
+ * mode - rootUrl : <meta name="rootUrl ... />
+ */
+commonLib.getMeta = function(mode) {
+    if(!mode) return;
+
+    const el = document.querySelector(`meta[name='${mode}']`);
+
+    return el?.content;
+};
+
+/**
+* Ajax 요청 처리
+*
+* @params url : 요청 주소, http[s] :외부 URL - 컨텍스트 경로는 추가 X
+* @params method 요청 방식 - GET, POST, DELETE, PATCH...
+* @params callback 응답 완료 후 후속 처리 콜백 함수
+* @params data : 요청 데이터
+* @params headers : 추가 요청 헤더
+*/
+commonLib.ajaxLoad = function(url, callback, method = "GET", data, headers) {
+    if (!url) return;
+
+    const { getMeta } = commonLib;
+
+    const csrfHeader = getMeta("_csrf_header");
+    const csrfToken = getMeta("_csrf");
+    url = /^http[s]?:/.test(url) ? url : getMeta("rootUrl") + url;
+
+    headers = headers ?? {};
+    headers[csrfHeader] = csrfToken;
+
+    // 통일성을 위해 모두 대문자화
+    method = method.toUpperCase();
+
+    const options = {
+        method,
+        headers,
+    }
+
+    if (data && method in ['POST', 'PUT', 'PATCH']) { // body 쪽 데이터 추가 기능
+        options.body = data instanceof FormData ? data : JSON.stringfy(data);
+    }
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+            if (json.success) { // 응답 성공(처리 성공)
+                if (typeof callback === "function") { // 콜백 함수가 정의된 경우
+                    callback(json.data);
+                }
+                return;
+            }
+
+            alert(json.message); // 실패 시에는 alert 메세지 출력
+        })
+        .catch(err => console.error(err))
+};
+
 /* 관리자, 프론트, 모바일의 공통적인 js파일 */
 window.addEventListener("DOMContentLoaded", function() {
     // 체크 박스 전체 토글 기능 S
--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -3,27 +3,25 @@ commonLib.fileManager = {
     /**
     * 파일 업로드 처리
     *
-    *
     */
     upload(files, gid, location, single, imageOnly) {
         try {
             /* 유효성 검사 S */
             if (!files || files.length === 0) {
-                throw new Error("파일을 선택하세요");
+                throw new Error("파일을 선택하세요.");
             }
 
             if (imageOnly) { // 이미지만 업로드 하는 경우
                 for (const file of files) {
-                    if (file.type.indexOf("image/") === -1) { // 이미지가 아닌 파일의 경우
+                    if (file.type.indexOf("image/") === -1) { // 이미지가 아닌 파일인 경우
                         throw new Error("이미지 형식이 아닙니다.");
                     }
                 }
             }
 
-            if (!gid || ('' + gid).trim()) {
+            if (!gid || !('' + gid).trim()) {
                 throw new Error("잘못된 접근입니다.");
             }
-
             /* 유효성 검사 E */
 
             /* 전송 양식 만들기 S */
@@ -31,50 +29,73 @@ commonLib.fileManager = {
             formData.append("gid", gid);
             formData.append("single", single);
             formData.append("imageOnly", imageOnly);
-
-            if(location) {
+            if (location) {
                 formData.append("location", location);
             }
 
             for (const file of files) {
                 formData.append("file", file);
             }
+
             /* 전송 양식 만들기 E */
 
+            /* 양식 전송 처리 S */
+            const { getMeta } = commonLib;
+
+            const csrfHeader = getMeta("_csrf_header");
+            const csrfToken = getMeta("_csrf");
+            const url = getMeta("rootUrl") + "api/file/upload";
+
+            fetch(url, {
+                method: "POST",
+                headers: {[csrfHeader]: csrfToken},
+                body: formData
+            })
+            .then(res => res.json())
+            .then(json => console.log(json));
+
+            /* 양식 전송 처리 E */
         } catch (err) {
             alert(err.message);
             console.error(err);
         }
     }
+
 };
 
 window.addEventListener("DOMContentLoaded", function() {
     const fileUploads = document.getElementsByClassName("file-upload");
-    const fileEl = document.createElement("input")
-    fileEl.type = 'file';
+    let fileEl = null;
 
     for (const el of fileUploads) {
         el.addEventListener("click", function() {
             const {gid, location, single, imageOnly} = this.dataset;
 
+            if (!fileEl) {
+                fileEl = document.createElement("input");
+                fileEl.type = 'file';
+            }
+
             fileEl.gid = gid;
             fileEl.location = location;
             fileEl.imageOnly = imageOnly === 'true';
             fileEl.single = single === 'true';
-            fileEl.multiple = !fileEl.single; // false - 단일 파일 선택, true - 여러 파일 선택 가능
+            fileEl.multiple = !fileEl.single;  // false - 단일 파일 선택, true - 여러파일 선택 가능
 
             fileEl.click();
+
+
+             // 파일 선택시 - change 이벤트 발생
+             fileEl.removeEventListener("change", fileEventHandler);
+             fileEl.addEventListener("change", fileEventHandler);
+
+             function fileEventHandler(e) {
+                const files = e.currentTarget.files;
+                const {gid, location, single, imageOnly} = fileEl;
+
+                const { fileManager } = commonLib;
+                fileManager.upload(files, gid, location, single, imageOnly);
+             }
         });
     }
-
-    // 파일 선택 시 - chage 이벤트 발생
-    fileEl.addEventListener("change", function(e){
-        // e.currentTarget, e.target
-        // console.dir(e.currentTarget.files)
-       const files = e.currentTarget.files;
-       const {gid, location, single, imageOnly} = fileEl;
-
-       const { fileManager } = commonLib;
-       fileManager.upload(files, gid, location, single, imageOnly);
-    });
 });
