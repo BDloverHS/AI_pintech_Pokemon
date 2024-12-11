1d761378cf92e28c7cf6a2626b8436d70059cf47
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-11 15:14:06 +0900

    페이징 테스트

--- src/main/java/org/koreait/global/paging/Pagination.java
+++ src/main/java/org/koreait/global/paging/Pagination.java
@@ -1,13 +1,34 @@
 package org.koreait.global.paging;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// Setter는 따로 필요하지 않으므로 @Data 대신 아래 두 어노테이션을 씀
+@Getter
+@ToString
 public class Pagination {
+    private int page;
+    private int total;
+    private int ranges;
+    private int limit;
+    private int totalPages;
+    private int firstRangePage;
+    private int lastRangePage;
+    private int prevLastLastPage;
+    private int nextRangeFirstPage;
+
     /**
      * @param page : 현재 페이지 번호
      * @param total : 총 레코드 갯수
      * @param ranges : 페이지 구간 갯수
      * @param limit : 한 페이지 당 출력될 레코드 갯수
      */
-    public Pagination(int page, int total, int ranges, int limit) {
+    public Pagination(int page, int total, int ranges, int limit, HttpServletRequest request) {
         // 페이징 기본값 처리
         page = Math.max(page, 1);
         total = Math.max(total, 0);
@@ -31,7 +52,41 @@ public class Pagination {
         int prevRageFirstPage = 0, nextRangeFirstPage = 0;
 
         if (rangeCnt > 0) {
-
+            prevRageFirstPage = (rangeCnt - 1) * ranges + ranges;
         }
+
+        // 마지막 페이지 구간
+        int lastRangeCnt = (totalPages - 1) / ranges;
+        if (rangeCnt < lastRangeCnt) {
+            nextRangeFirstPage = (rangeCnt + 1) * ranges + 1;
+        }
+
+        this.page = page;
+        this.total = total;
+        this.ranges = ranges;
+        this.limit = limit;
+        this.totalPages = totalPages;
+        this.firstRangePage = firstRangePage;
+        this.lastRangePage = lastRangeCnt;
+        this.prevLastLastPage = prevRageFirstPage;
+        this.nextRangeFirstPage = nextRangeFirstPage;
+    }
+
+    /**
+     * String 배열의 0번째 - 페이지 번호 숫자, 1번째 - 페이지 URL
+     * @return
+     */
+    public List<String[]> getPages() {
+        if (total == 0) {
+            return Collections.EMPTY_LIST;
+        }
+
+        List<String[]> pages = new ArrayList<>();
+        for (int i = firstRangePage; i < lastRangePage; i++) {
+            String url = "?pages=" + i;
+            pages.add(new String[] {"" + i, url});
+        }
+
+        return pages;
     }
 }
---
+++ src/test/java/org/koreait/global/paging/PaginationTest.java
@@ -0,0 +1,35 @@
+package org.koreait.global.paging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock; // 가짜 객체
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+
+import static org.mockito.BDDMockito.*;
+
+@SpringBootTest
+@ActiveProfiles({"default","test"})
+public class PaginationTest {
+
+    // 가짜 객체
+    @Mock
+    private HttpServletRequest request;
+
+    @BeforeEach
+    void init() {
+        // Stub(스텁 - 가짜 데이터)
+        given(request.getQueryString()).willReturn("query=블로그&test1=1&test2=2");
+    }
+
+    @Test
+    void test1() {
+        Pagination paginaion = new Pagination(23, 9999, 10, 20, request);
+        System.out.println(paginaion);
+
+        paginaion.getPages().forEach(s -> System.out.println(Arrays.toString(s)));
+    }
+}
