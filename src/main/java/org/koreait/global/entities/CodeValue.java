1dde3833ebd418c01b4dcc1be0cc5e00d740955f
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-23 17:20:38 +0900

    CodeValue 코드 추가

---
+++ src/main/java/org/koreait/global/entities/CodeValue.java
@@ -0,0 +1,24 @@
+package org.koreait.global.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeValue {
+
+    @Id
+    @Column(name="_CODE", length=45) // db에서 code가 예약어이므로 디비에서의 이름을 지어즘
+    private String code;
+
+    @Lob
+    @Column(name="_VALUE")
+    private String value;
+}
---
+++ src/main/java/org/koreait/global/repositories/CodeValueRepository.java
@@ -0,0 +1,8 @@
+package org.koreait.global.repositories;
+
+import org.koreait.global.entities.CodeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeValueRepository extends JpaRepository<CodeValue, String> {
+
+}
