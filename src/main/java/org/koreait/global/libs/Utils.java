fcf466fd5a1966737842fd2241d0baffc69c1345
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-24 16:04:15 +0900

    관리자 페이지 사이트 기본 정보 완료

--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -172,4 +172,10 @@ public class Utils {
         HttpSession session = request.getSession();
         session.setAttribute("showMessage", message);
     }
+
+
+    public void removeSessionMessage() {
+        HttpSession session = request.getSession();
+        session.removeAttribute("showMessage");
+    }
 }
