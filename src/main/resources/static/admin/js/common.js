e787aa78770c8c3d9a27484a4e41683e5183135c
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-26 22:41:13 +0900

    오타 및 간격 수정

--- src/main/resources/static/admin/js/common.js
+++ src/main/resources/static/admin/js/common.js
@@ -1,5 +1,6 @@
 /* 관리자 쪽에서의 공통적인 js파일 */
 window.addEventListener("DOMContentLoaded", function() {
+    /* 공통 양식 버튼 처리 S */
     const tableActions = document.getElementsByClassName("table-action");
     for (const tableAction of tableActions) {
         const { formName } = tableAction.dataset;
@@ -9,19 +10,20 @@ window.addEventListener("DOMContentLoaded", function() {
         const buttons = frm.querySelectorAll("button");
 
         for (const button of buttons) {
-            button,addEventListener("click", function() {
+            button.addEventListener("click", function() {
                 let method = "PATCH";
                 if (this.classList.contains("delete")) {
                     method = "DELETE";
                 }
 
-                if (confirm(`정말 ${method === 'DELETE' ? '삭제' : '수정'}하겠습니까?`)) {
+                if (confirm(`정말 ${method === 'DELETE' ? '삭제' : '수정'} 하겠습니까?`)) {
                     frm._method.value = method;
                     frm.submit();
                 }
             });
         }
+
     }
 
-    // 공통 양식 버튼 처리
+    // 공통 양식 버튼 처리 E
 });
