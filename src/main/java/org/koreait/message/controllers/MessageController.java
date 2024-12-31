b3c3fdf75f9f0bcad6959853271c2e9a39250d31
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-31 13:14:14 +0900

    메서지 컨트롤러 수정(DeleteMapping 추가)

--- src/main/java/org/koreait/message/controllers/MessageController.java
+++ src/main/java/org/koreait/message/controllers/MessageController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.global.annotations.ApplyErrorPage;
 import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @ApplyErrorPage
@@ -33,7 +32,7 @@ public class MessageController {
      */
     @PostMapping
     public String process() {
-        return "redirect:/message/lists";
+        return "redirect:/message/list";
     }
 
     /**
@@ -49,5 +48,10 @@ public class MessageController {
     @GetMapping("/view/{seq}")
     public String info(@PathVariable("seq") Long seq) {
         return utils.tpl("message/view");
+    }
+
+    @DeleteMapping
+    public String delete(@RequestParam(name="seq", required=false) List<String> seq) {
+        return "redirect:/message/list";
     }
 }
