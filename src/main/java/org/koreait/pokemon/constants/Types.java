b2a224bdf505dab7d606483b7644bf096b99d678
BDloverHS <gustn020702naver.com@gmail.com>
2024-12-31 10:45:12 +0900

    타입을 위한 이넘 클래스

---
+++ src/main/java/org/koreait/pokemon/constants/Types.java
@@ -0,0 +1,22 @@
+package org.koreait.pokemon.constants;
+
+public enum Types {
+    NORMAL, // 노말
+    FIRE, // 불꽃
+    WATER, // 물
+    GRASS, // 땅
+    ELECTRIC, // 전기
+    ICE, // 얼음
+    FIGHTING, // 격투
+    POISON, // 독
+    GROUND, // 땅
+    FLYING, // 비행
+    PSYCHIC, // 에스퍼
+    BUG, // 벌레
+    ROCK, // 바위
+    GHOST, // 고스트
+    DRAGON, // 드래곤
+    DARK, // 악
+    STEEL, // 강철
+    FAIRY // 페어리
+}
