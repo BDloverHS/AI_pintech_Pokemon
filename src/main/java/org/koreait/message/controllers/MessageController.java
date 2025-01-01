2d3b3ae4303d6d6cbae39e76025d85a31ea3d9d1
BDloverHS <gustn020702naver.com@gmail.com>
2025-01-01 16:24:11 +0900

    ckeditor5 js 공통 기능 추가

--- src/main/java/org/koreait/message/controllers/MessageController.java
+++ src/main/java/org/koreait/message/controllers/MessageController.java
@@ -107,6 +107,7 @@ public class MessageController {
         if (mode.equals("send")) { // 쪽지 보내기
             pageTitle = utils.getMessage("쪽지 보내기");
             addCommonScript.add("fileManager");
+            addCommonScript.add("ckeditor5/ckeditor");
             addScript.add("message/send");
         }
 
